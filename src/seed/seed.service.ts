d043e8fdca2780ae68547acce68d687dd8bb2ebf
jrmart12 <rodolfojose@live.com>
2024-07-11 01:10:25 -0600

    after inserting by lot

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModule: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=600',
+      'https://pokeapi.co/api/v2/pokemon?limit=100',
     );
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-
-      console.log(name, no);
+      await this.pokemonModule.create({ name, no });
     });
-    return data.results;
+    return 'Seed executed';
   }
 }
